7511cd3e5de9c63ddbb0bce2f40701f2127eaad5
Bello Emmanuel Oluwatimilehin <Timilehin-bello@users.noreply.github.com>
2020-09-09 13:35:38 +0100

    Deploy

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ class App extends Component {
   
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input})
-    fetch('http://localhost:3001/imageurl', {
+    fetch('https://aqueous-eyrie-30614.herokuapp.com/imageurl', {
       method: 'post',
       headers : {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -101,7 +101,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if(response) {
-        fetch('http://localhost:3001/image', {
+        fetch('https://aqueous-eyrie-30614.herokuapp.com/image', {
           method: 'put',
           headers : {'Content-Type': 'application/json'},
           body: JSON.stringify({
--- src/component/Signin/Signin.js
+++ src/component/Signin/Signin.js
@@ -16,7 +16,7 @@ class  Signin extends Component {
         this.setState({signInPassword: event.target.value})
     }
     onSubmitSignIn = () => {
-        fetch('http://localhost:3001/signin', {
+        fetch('https://aqueous-eyrie-30614.herokuapp.com/signin', {
             method: 'post',
             headers : {'Content-Type': 'application/json'},
             body: JSON.stringify({
